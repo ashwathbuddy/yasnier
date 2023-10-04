0a13fc44c84e58f13c175a54de266f52774e8293
Sergio <sbravog86@gmail.com>
2023-10-04 00:54:56 -0400

    Implementing getDevices

--- src/ui/src/app/core/private/private.service.ts
+++ src/ui/src/app/core/private/private.service.ts
@@ -1,11 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Device } from '../../../../../../libs/data-model/src/models';
+import { IResponse } from '../../../../../../libs/api-interfaces/src';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PrivateService {
-  constructor() {
-    http: HttpClient;
+  private apiUrl = '/api/devices';
+
+  constructor(private http: HttpClient) {}
+
+  getDevices(accountId: string): Observable<Device[]> {
+    const url = `${this.apiUrl}/${accountId}`;
+    return this.http.get<IResponse<Device[]>>(url).pipe(
+      map(({ data }: { data: Device[] }) => {
+        return data;
+      }),
+    );
   }
 }
--- src/ui/src/app/modules/private/private.component.ts
+++ src/ui/src/app/modules/private/private.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Device } from '../../../../../../libs/data-model/src/models';
+import { PrivateService } from 'src/app/core/private/private.service';
+import { AuthService } from 'src/app/core/auth/auth.service';
 
 @Component({
   selector: 'app-private',
@@ -8,4 +11,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './private.component.html',
   styleUrls: ['./private.component.scss'],
 })
-export class PrivateComponent {}
+export class PrivateComponent implements OnInit {
+  devices: Device[] = [];
+  accountId = '';
+
+  constructor(private privateService: PrivateService, private authService: AuthService) {
+    this.authService['userSubject'].subscribe((data) => {
+      this.accountId = data?.id as unknown as string;
+    });
+  }
+
+  ngOnInit(): void {
+    this.privateService.getDevices(this.accountId).subscribe((devices) => {
+      this.devices = devices;
+      console.log('Devices:', this.devices);
+    });
+  }
+
+  // deleteDevice(deviceId: string): void {
+  // Aquí puedes implementar la lógica para eliminar el dispositivo por su ID
+  // Puedes utilizar el servicio DeviceService para realizar la solicitud DELETE a la API
+  // }
+}
